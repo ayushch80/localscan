bd1aaadb5340fcda20ac6f9cac93ff0161c4363a
Ayush Choudhary <87753540+ayushch80@users.noreply.github.com>
2022-06-10 15:40:52 +0530

    Update localscan.js

--- js/localscan.js
+++ js/localscan.js
@@ -1,4 +1,7 @@
 import { ethers } from "https://cdn.ethers.io/lib/ethers-5.2.esm.min.js";
 const bData = document.getElementById("uData");
 const rpcUrl = bData.elements.namedItem("rpc").value;
+const blockData = bData.elements.namedItem("block").value;
+const txnHash = bData.elements.namedItem("txn").value;
+const addressData = bData.elements.namedItem("address").value;
 console.log(rpcUrl)

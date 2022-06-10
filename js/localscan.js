ab6aa94b3d72043fd6f01668e4b89c0b06ed0265
Ayush Choudhary <87753540+ayushch80@users.noreply.github.com>
2022-06-10 15:30:21 +0530

    Update localscan.js

--- js/localscan.js
+++ js/localscan.js
@@ -1,4 +1,4 @@
-import { ethers } from "/ethers.js";
+import { ethers } from "https://cdn.ethers.io/lib/ethers-5.2.esm.min.js";
 const bData = document.getElementById("uData");
 const rpcUrl = bData.elements.namedItem("rpc").value;
 console.log(rpcUrl)

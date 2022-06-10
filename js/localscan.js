dd5aa835c2fb7c750a2bb0db322fd08828476db0
Ayush Choudhary <87753540+ayushch80@users.noreply.github.com>
2022-06-10 21:03:22 +0530

    Update localscan.js

--- js/localscan.js
+++ js/localscan.js
@@ -1,13 +1,14 @@
 import { ethers } from "https://cdn.ethers.io/lib/ethers-5.2.esm.min.js";
 
-const bData = document.getElementById("uData");
-const rpcUrl = bData.elements.namedItem("rpc").value;
-const blockData = bData.elements.namedItem("block").value;
-const txnHash = bData.elements.namedItem("txn").value;
-const addressData = bData.elements.namedItem("address").value;
-
 document.getElementById("btn_click").addEventListener("click", getData);
 function getData() {
+  
+  const bData = document.getElementById("uData");
+  const rpcUrl = bData.elements.namedItem("rpc").value;
+  const blockData = bData.elements.namedItem("block").value;
+  const txnHash = bData.elements.namedItem("txn").value;
+  const addressData = bData.elements.namedItem("address").value;
+
   const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
   const block = await provider.getBlock(blockData)
 

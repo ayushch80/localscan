3b9d0ac390718e142a3f37d781de8822b53d7789
Ayush Choudhary <87753540+ayushch80@users.noreply.github.com>
2022-06-10 16:24:41 +0530

    Update localscan.js

--- js/localscan.js
+++ js/localscan.js
@@ -1,9 +1,17 @@
 import { ethers } from "https://cdn.ethers.io/lib/ethers-5.2.esm.min.js";
+
 const bData = document.getElementById("uData");
-const rpcUrl = bData.elements.namedItem("rpc").value;  const blockData = bData.elements.namedItem("block").value;
+const rpcUrl = bData.elements.namedItem("rpc").value;
+const blockData = bData.elements.namedItem("block").value;
 const txnHash = bData.elements.namedItem("txn").value;
 const addressData = bData.elements.namedItem("address").value;
+
+const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
+const blockData = await provider.getBlock(bData)
+
+const blockHash = blockData.hash
 document.getElementById("btn_click").addEventListener("click", getData);
 function getData() {
-  $('.rpcU').append("<b>ENTERED JSON RPC URL --><b>" + rpcUrl);
+  $('.rpcU').append("<p><b>ENTERED JSON RPC URL --></b>" + rpcUrl + "</p>");
+  $('.blockD').append("<h4>BLOCK DATA</h4><p><b>Block Number</b>" + blockData + "<br><b>BLOCK HASH</b>" + blockHash + "</p>");
 }
